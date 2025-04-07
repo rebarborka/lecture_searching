df3efa66c376b045612996d7a9b43bc3f7490eb7
Barbora Trnkova <barbortrnkova@gmail.com>
2025-04-07 12:58:33 +0200

    cvicenie

--- searching.py
+++ searching.py
@@ -11,10 +11,31 @@ def read_data(file_name, field):
     :param field: (str), field of a dict to return
     :return: (list, string),
     """
+    import json
+
+    if field not in {"unordered_numbers","ordered_numbers","dna_sequence"}:
+        return None
+
     file_path = os.path.join(cwd_path, file_name)
+
+    with open(file_path, "r") as json_file:
+        sekvencia = json.load(json_file)
+
+    return sekvencia[field]
+
+
+# def linear_search(sekvencia, cislo):
+#     slovnik = {}
+#     zoznam_pozic = []
+#     for i in sekvencia:
+#         if
+
 
 
 def main():
+    file_name = "sequential.json"
+    sekvencia = read_data(file_name, field="unordered_numbers")
+    print(sekvencia)
     pass
 
 
